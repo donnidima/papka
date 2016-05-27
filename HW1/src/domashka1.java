db572b84161f8f0a3e990080b508344287a0d8ea
donnidima <donni.dima@gmail.com>
2016-05-27 17:46:52 +0300

    init commit

---
+++ HW1/src/domashka1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by donni on 23.05.2016.
+ */
+public class domashka1{
+    public static void main(String[] args){
+        int n=287;
+        System.out.println("Сумма цифр " +n + " = " + ((n%10)+((n/10)%10)+((n/100)%10)));
+    }
+}
