ff1db02f0edf06a0ff7915485d6172a11a0e5772
donnidima <donni.dima@gmail.com>
2016-05-27 17:49:48 +0300

    init commit

---
+++ HW2/src/okrugleniya.java
@@ -0,0 +1,9 @@
+/**
+ * Created by donni on 23.05.2016.
+ */
+public class okrugleniya {
+    public static void main (String[] args) {
+        double n = 3.46;
+        System.out.println("Если округлить число " +n+ " , то получим   " +(int)n);
+    }
+}
